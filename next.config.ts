adc079acfa56031d358f73f3d81e29b944e48ca4
dlampatricio <dlampatricio@gmail.com>
2026-02-17 14:52:20 -0500

    feat: app v3 - deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Genera archivos estáticos compatibles con GitHub Pages
+  output: 'export',
+  
+  // Desactiva la optimización de imágenes (GitHub no tiene servidor de Node para esto)
+  images: {
+    unoptimized: true,
+  },
+  
+  // Opcional: Ayuda a evitar errores de hidratación comunes en despliegues estáticos
+  reactStrictMode: true,
 };
 
 export default nextConfig;
